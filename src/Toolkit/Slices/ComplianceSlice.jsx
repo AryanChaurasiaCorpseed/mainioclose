18be3316a159ee6e3aaeb50cc8230d1d487b3a31
Avnishbharti <kaushlendra.pratap@corpseed.com>
2024-11-12 10:04:36 +0530

    minor changes in compliances

--- src/Toolkit/Slices/ComplianceSlice.jsx
+++ src/Toolkit/Slices/ComplianceSlice.jsx
@@ -1,39 +1,49 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getQuery } from "../../API/GetQuery";
-import { postQuery } from "../../API/PostQuery";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { getQuery } from "../../API/GetQuery"
+import { postQuery } from "../../API/PostQuery"
 
-
-export const getAllCompliances=createAsyncThunk('getCompliances',async()=>{
-    const response=await getQuery(`/leadService/api/v1/complianceDocumnets/getAllComplianceDocuments`)
+export const getAllCompliances = createAsyncThunk(
+  "getCompliances",
+  async () => {
+    const response = await getQuery(
+      `/leadService/api/v1/complianceDocumnets/getAllComplianceDocuments`
+    )
     return response.data
-})
+  }
+)
 
-export const uploadDocuments=createAsyncThunk(`uploadDocuments`,async(data)=>{
-    const response=await postQuery(`/leadService/api/v1/complianceDocumnets/createDocuments`,data)
+export const uploadDocuments = createAsyncThunk(
+  `uploadDocuments`,
+  async (data) => {
+    const response = await postQuery(
+      `/leadService/api/v1/complianceDocumnets/createDocuments?categoryId=${data?.categoryId} `,
+      data
+    )
     return response.data
-})
+  }
+)
 
-const complianceSlice=createSlice({
-    name:'compliance',
-    initialState:{
-        allCompliance:[],
-        complianceLoading:false,
-        complianceError:false
-    },
-    reducers:{},
-    extraReducers:(builder)=>{
-        builder.addCase(getAllCompliances.pending,(state,action)=>{
-            state.complianceLoading=true
-        })
-        builder.addCase(getAllCompliances.fulfilled,(state,action)=>{
-            state.allCompliance=action.payload
-            state.complianceLoading=false
-            state.complianceError=false
-        })
-        builder.addCase(getAllCompliances.rejected,(state,action)=>{
-            state.complianceLoading=true
-        })
-    }
+const complianceSlice = createSlice({
+  name: "compliance",
+  initialState: {
+    allCompliance: [],
+    complianceLoading: false,
+    complianceError: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllCompliances.pending, (state, action) => {
+      state.complianceLoading = true
+    })
+    builder.addCase(getAllCompliances.fulfilled, (state, action) => {
+      state.allCompliance = action.payload
+      state.complianceLoading = false
+      state.complianceError = false
+    })
+    builder.addCase(getAllCompliances.rejected, (state, action) => {
+      state.complianceLoading = true
+    })
+  },
 })
 
 // const {}=complianceSlice.actions
