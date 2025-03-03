8b0a4b9bdf18944dce7bf8659e3e0a07768d1e75
avnishkumarcorp <avnish.kumar@corpseed.com>
2025-03-03 11:45:18 +0530

    some minor changes

--- src/API/PutQuery.jsx
+++ src/API/PutQuery.jsx
@@ -1,16 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
-const localData = JSON.parse(JSON.parse(localStorage?.getItem("persist:root")).auth);
+const localData = JSON.parse(
+  JSON.parse(localStorage?.getItem("persist:root")).auth
+);
 
-export const putQuery = (URL,data) =>{
-    return(
-        axios.put(URL, {
-            ...data,
-            headers: {
-                "Authorization": `Bearer ${localData?.jwt}`,
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "application/json",
-              },
-        })
-    )
-}
+export const putQuery = (URL, data) => {
+  return axios.put(URL, data, {
+    headers: {
+      Authorization: `Bearer ${localData?.jwt}`,
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  });
+};
